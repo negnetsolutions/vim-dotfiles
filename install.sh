231bb9ff2d49905a645ed386a3e8ff564eb95ae4
Andrew Johnson <andy@negnetsolutions.com>
2011-12-26 12:53:22 -0600

    Updated install scripts to compile command-t

--- install.sh
+++ install.sh
@@ -6,3 +6,8 @@ ln -s ~/.vim/vimrc ~/.vimrc
 cd ~/.vim
 git submodule init
 git submodule update
+
+# update command-t
+cd bundle/wincent-Command-T
+rake make
+cd ~/.vim
--- update_modules.sh
+++ update_modules.sh
@@ -3,3 +3,8 @@
 cd ~/.vim
 git pull origin master
 git submodule foreach git pull origin master
+
+# update command-t
+cd bundle/wincent-Command-T
+rake make
+cd ~/.vim
