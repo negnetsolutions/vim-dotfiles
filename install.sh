5668c96f628b37acc80434da40c769b5e5d337b8
Andrew Johnson <andy@negnetsolutions.com>
2014-03-11 14:03:22 -0500

    stupid

--- install.sh
+++ install.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-cd ~
-ln -s ~/.vim/vimrc ~/.vimrc
-cd ~/.vim
+# cd ~
+# ln -s ~/.vim/vimrc ~/.vimrc
+# cd ~/.vim
 git submodule init
-git submodule update
+git submodule update --recursive
 
 # update command-t
 cd bundle/othree-html5
--- update.sh
+++ update.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 git pull origin `git branch | sed -n '/\* /s///p'`
+git submodule init
 git submodule sync
-git submodule update --init --recursive --rebase
-git submodule foreach git pull origin master
+# git submodule update --recursive
+# git submodule foreach git pull origin master
+git submodule foreach 'git fetch origin --tags; git checkout master; git pull' && git pull && git submodule update --init --recursive
 # git submodule update
 
-cd bundle
-
 # update html5
-cd othree-html5
+cd bundle/othree-html5
 make install
 cd ../..
