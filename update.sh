775467740fe4a76888f7fbdb5a814c657f171e89
Andrew Johnson <andy@negnetsolutions.com>
2014-03-26 12:17:19 -0500

    Removed comments in update and install scripts. Removed extra install script?? Why was that there?

--- update.sh
+++ update.sh
@@ -3,12 +3,4 @@
 git pull origin `git branch | sed -n '/\* /s///p'`
 git submodule init
 git submodule sync
-# git submodule update --recursive
-# git submodule foreach git pull origin master
 git submodule foreach 'git fetch origin --tags; git checkout master; git pull' && git pull && git submodule update --init --recursive
-# git submodule update
-
-# update html5
-cd bundle/othree-html5
-make install
-cd ../..
