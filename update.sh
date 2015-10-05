11e02b9e359f7dbd4ac5ee821ac0e5e4b88fb004
Andrew Johnson <andy@negnetsolutions.com>
2015-10-04 19:41:08 -0500

    Script update

--- update.sh
+++ update.sh
@@ -3,4 +3,5 @@
 git pull origin `git branch | sed -n '/\* /s///p'`
 git submodule init
 git submodule sync
-git submodule foreach 'git fetch origin --tags; git checkout master; git pull origin master' && git pull origin master && git submodule update --init --recursive
+git submodule update --remote --merge
+git add . && git commit -m "Submodule updates"
