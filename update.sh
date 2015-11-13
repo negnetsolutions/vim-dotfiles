e33b2f05396cbe920a2f2a54e5ff4cfc05a427f3
Andrew Johnson <andy@negnetsolutions.com>
2015-11-13 10:25:12 -0600

    Remove update.sh

--- update.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-git pull origin `git branch | sed -n '/\* /s///p'`
-git submodule init
-git submodule sync
-git submodule update --remote --merge
-git add . && git commit -m "Submodule updates"
