8c1c98e3e75a2b3609be6b42709b2d4df7e95c69
Andrew Johnson <andy@negnet.co>
2022-09-22 18:04:43 -0500

    Remove animations from windows.nvim

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -125,14 +125,13 @@ return require("packer").startup(function(use)
     use { "anuvyklack/windows.nvim",
       requires = {
         "anuvyklack/middleclass",
-        "anuvyklack/animation.nvim"
       },
       config = function()
         vim.o.winwidth = 10
         vim.o.winminwidth = 10
         vim.o.equalalways = false
         require('windows').setup()
-
+    
         vim.keymap.set('n', '<C-w>z', '<Cmd>WindowsMaximize<CR>')
       end
     }
