81044c8b0f24fd391c47f79681c305ef07f64d38
Andrew Johnson <andrew@libiny.com>
2023-04-23 09:40:33 -0500

    Add neo-tree.

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -29,6 +29,20 @@ require("packer").init({
 return require("packer").startup(function(use)
     use({ "wbthomason/packer.nvim" })
 
+    -- Unless you are still migrating, remove the deprecated commands from v1.x
+    vim.cmd([[ let g:neo_tree_remove_legacy_commands = 1 ]])
+
+    -- Add neo-tree
+    use {
+      "nvim-neo-tree/neo-tree.nvim",
+      branch = "v2.x",
+      requires = { 
+        "nvim-lua/plenary.nvim",
+        "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+        "MunifTanjim/nui.nvim",
+      }
+    }
+
     -- Text manipulation
     use({ "tpope/vim-surround" })
     use({ "tpope/vim-repeat" })
