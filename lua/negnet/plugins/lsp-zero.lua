5637f895287646a03d08f9b5b1f5c486da318a6b
Andrew Johnson <andrew@libiny.com>
2023-09-25 08:29:08 -0500

    Improve

--- lua/negnet/plugins/lsp-zero.lua
+++ lua/negnet/plugins/lsp-zero.lua
@@ -9,11 +9,6 @@ return {
 
     -- Autocompletion
     { 'hrsh7th/nvim-cmp' },
-    { 'hrsh7th/cmp-buffer' },
-    { 'hrsh7th/cmp-path' },
-    { 'saadparwaiz1/cmp_luasnip' },
-    { 'hrsh7th/cmp-nvim-lsp' },
-    { 'hrsh7th/cmp-nvim-lua' },
 
     -- Snippets
     { 'L3MON4D3/LuaSnip' },
@@ -99,8 +94,8 @@ return {
         intelephense = {
           files = {
             exclude = { "**/phpstan/resultCache.php", "**/.git/**", "**/.svn/**", "**/.hg/**", "**/CVS/**",
-            "**/.DS_Store/**", "**/node_modules/**", "**/bower_components/**", "**/vendor/**/{Tests,tests}/**",
-            "**/.history/**", "**/vendor/**/vendor/**" }
+              "**/.DS_Store/**", "**/node_modules/**", "**/bower_components/**", "**/vendor/**/{Tests,tests}/**",
+              "**/.history/**", "**/vendor/**/vendor/**" }
           },
         },
       }
---
+++ lua/negnet/plugins/nvim-cmp.lua
@@ -0,0 +1,11 @@
+return {
+  'hrsh7th/nvim-cmp',
+  event = 'InsertEnter',
+  dependencies = {
+    { 'hrsh7th/cmp-buffer' },
+    { 'hrsh7th/cmp-path' },
+    { 'saadparwaiz1/cmp_luasnip' },
+    { 'hrsh7th/cmp-nvim-lsp' },
+    { 'hrsh7th/cmp-nvim-lua' },
+  },
+}
