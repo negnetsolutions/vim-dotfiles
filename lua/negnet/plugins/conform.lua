b9315e28a884ac0a4095a58bd0d77c9e001e6c6c
Andrew Johnson <andrew@libiny.com>
2023-12-18 08:46:08 -0600

    Remove null-ls

---
+++ lua/negnet/plugins/conform.lua
@@ -0,0 +1,29 @@
+return {
+	"stevearc/conform.nvim",
+	event = { "BufReadPre", "BufNewFile" },
+	config = function()
+		local conform = require("conform")
+
+		conform.setup({
+			formatters_by_ft = {
+				javascript = { "eslint" },
+				typescript = { "eslint" },
+				javascriptreact = { "eslint" },
+				typescriptreact = { "eslint" },
+				css = { "prettier" },
+				html = { "prettier" },
+				json = { "prettier" },
+				yaml = { "prettier" },
+				markdown = { "prettier" },
+				graphql = { "prettier" },
+				lua = { "stylua" },
+				php = { "phpcbf" },
+			},
+			format_on_save = {
+				lsp_fallback = true,
+				async = false,
+				timeout_ms = 500,
+			},
+		})
+	end,
+}
--- lua/negnet/plugins/lsp-zero.lua
+++ lua/negnet/plugins/lsp-zero.lua
@@ -26,20 +26,19 @@ return {
     })
 
     lsp.on_attach(function(client, bufnr)
-      if client.supports_method("textDocument/formatting") then
-        vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
-        vim.api.nvim_create_autocmd("BufWritePre", {
-          group = augroup,
-          buffer = bufnr,
-          callback = function()
-            vim.lsp.buf.format({
-              filter = function(client1) return client1.name ~= "intelephense" end,
-              bufnr = bufnr,
-            })
-          end,
-        })
-      end
-
+      -- if client.supports_method("textDocument/formatting") then
+      --   vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+      --   vim.api.nvim_create_autocmd("BufWritePre", {
+      --     group = augroup,
+      --     buffer = bufnr,
+      --     callback = function()
+      --       vim.lsp.buf.format({
+      --         filter = function(client1) return client1.name ~= "intelephense" end,
+      --         bufnr = bufnr,
+      --       })
+      --     end,
+      --   })
+      -- end
 
       local function has_value(tab, val)
         for index, value in ipairs(tab) do
