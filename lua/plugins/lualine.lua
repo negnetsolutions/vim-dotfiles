8bbbd7cc9ed28ab75c8141c9def6edde6ed4b42c
Andrew Johnson <andrew@libiny.com>
2024-01-11 10:51:35 -0600

    Add filetype, encoding, and fileformat to lualine

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -2,6 +2,13 @@ return {
   "nvim-lualine/lualine.nvim",
   opts = {
     sections = {
+      lualine_y = {
+        { "encoding" },
+        { "fileformat" },
+        { "filetype" },
+        { "progress", separator = " ", padding = { left = 1, right = 0 } },
+        { "location", padding = { left = 0, right = 1 } },
+      },
       lualine_z = {
         { "tabnine" },
         function()
