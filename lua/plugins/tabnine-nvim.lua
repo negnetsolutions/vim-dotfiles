6e3f80b844a79c513fb05f3450a0b10b9a37c070
Andrew Johnson <andrew@libiny.com>
2024-01-16 15:23:59 -0600

    Fix tabnine

--- lua/plugins/tabnine-nvim.lua
+++ lua/plugins/tabnine-nvim.lua
@@ -3,12 +3,12 @@ return {
   build = "./dl_binaries.sh",
   config = function()
     require("tabnine").setup({
-      -- disable_auto_comment = false,
-      accept_keymap = "<C-n>",
+      disable_auto_comment = true,
+      accept_keymap = "<C-t>",
       dismiss_keymap = "<C-]>",
       debounce_ms = 800,
       suggestion_color = { gui = "#808080", cterm = 244 },
-      exclude_filetypes = { "TelescopePrompt" },
+      exclude_filetypes = { "TelescopePrompt", "NvimTree" },
       log_file_path = nil, -- absolute path to Tabnine log file
     })
   end,
